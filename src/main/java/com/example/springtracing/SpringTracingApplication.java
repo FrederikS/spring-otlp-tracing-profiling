236bca06d34279ea6341c0bed90fde3cdb35c766
frederik.steffen <frederik.steffen@bayes.gg>
2023-05-12 12:06:42 +0200

    minor tweaks

--- src/main/java/com/example/springtracing/SpringTracingApplication.java
+++ src/main/java/com/example/springtracing/SpringTracingApplication.java
@@ -1,29 +1,13 @@
 package com.example.springtracing;
 
-import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
-import io.opentelemetry.sdk.trace.export.SpanExporter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-
-import static com.example.springtracing.SpringTracingApplication.*;
 
 @SpringBootApplication
-@EnableConfigurationProperties(AppConfig.class)
 public class SpringTracingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringTracingApplication.class, args);
     }
-
-    @Bean
-    public SpanExporter spanExporter(AppConfig config) {
-        return OtlpGrpcSpanExporter.builder().setEndpoint(config.otlpEndpoint()).build();
-    }
-
-    @ConfigurationProperties("app.config")
-    record AppConfig(String otlpEndpoint) {}
 
 }
